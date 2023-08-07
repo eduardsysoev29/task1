4e66afe686648fb9a9c523c8a549833e7e02ee80
Eduard Sysoev <krikisosedey@bk.ru>
2023-08-07 21:28:22 +0300

    add changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿Console.WriteLine("Введите 2 числа,");
+Console.WriteLine("чтобы узнать какое из них большее, а какое меньшее.");
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Число " + num1 + " большее");
+    Console.Write("Число " + num2 + " меньшее");
+}
+else if (num1 == num2)
+{
+    Console.WriteLine("Число " + num1 + " равно числу " + num2);
+}
+else
+{
+    Console.WriteLine("Число " + num2 + " большее");
+    Console.Write("Число " + num1 + " меньшее");
+}
